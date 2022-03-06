72ca21632d0f1088fc5873ba51e42ee9d247a0cc
MercuryKTgit <kimdotnetpossible@att.net>
2022-03-06 15:06:13 -0600

    Add Heroku

---
+++ server.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const { animals } = require('./data/animals.json');
+const PORT = 3002;
+// To instantiate the server
+const app = express();
+
+function filterByQuery(query, animalsArray) {
+  let personalityTraitsArray = [];
+  // Note that we save the animalsArray as filteredResults here:
+  let filteredResults = animalsArray;
+  if (query.personalityTraits) {
+    // Save personalityTraits as a dedicated array.
+    // If personalityTraits is a string, place it into a new array and save.
+    if (typeof query.personalityTraits === 'string') {
+      personalityTraitsArray = [query.personalityTraits];
+    } else {
+      personalityTraitsArray = query.personalityTraits;
+    }
+    // Loop through each trait in the personalityTraits array:
+    personalityTraitsArray.forEach(trait => {
+      // the filteredResults array will then contain only the entries that contain the trait,
+      // so at the end we'll have an array of animals that have every one 
+      // of the traits when the .forEach() loop is finished.
+      filteredResults = filteredResults.filter(
+        animal => animal.personalityTraits.indexOf(trait) !== -1
+      );
+    });
+  }
+  if (query.diet) {
+    filteredResults = filteredResults.filter(animal => animal.diet === query.diet);
+  }
+  if (query.species) {
+    filteredResults = filteredResults.filter(animal => animal.species === query.species);
+  }
+  if (query.name) {
+    filteredResults = filteredResults.filter(animal => animal.name === query.name);
+  }
+  // return the filtered results:
+  return filteredResults;
+}
+
+// parameter-1: a string that describes the route the client will have to fetch from
+// parameter-2: callback function that will execute every time that route is accessed with a GET request
+app.get('/api/animals', (req, res) => { // res.json(animals); //res parameter (short for response) to send the string Hello! to our client.
+    let results = animals;
+    console.log('-----------------')  
+    console.log(req.query) // print { name: 'Erica' }
+    console.log('-----------------')  
+    if (req.query) {
+      results = filterByQuery(req.query, results);
+    }
+    res.json(results); // send the result to the browser
+    // console.log(results); // print the json data
+   
+});
+
+// need to use listen() method onto our server to make our server listen
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}`);
+  console.log(`Example app listening at http://localhost:${PORT}/api/animals`)
+ });
